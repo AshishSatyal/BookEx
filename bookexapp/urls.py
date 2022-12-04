8fc800a5fadeac3e90fe045cef13da4cd3ab97c0
Shrijan Lakhey <lakheyshrijan@gmail.com>
2022-12-04 08:04:18 +0545

    fixed small error: changed url_patterns to urlpatterns in bookexapp urls.py file

--- bookexapp/urls.py
+++ bookexapp/urls.py
@@ -3,7 +3,7 @@ from bookexapp import views
 
 # app_name = "bookexapp"
 
-url_patterns = [
+urlpatterns = [
     path('user_signup/', views.user_signup, name="user_signup"),
     path('landingpage/', views.landingpage, name="landingpage"),
 ]
