99ad3dd0475c46725df087371868270847504ce9
Kanak-dev <kanak24.fv@gmail.com>
2022-12-16 18:24:03 +0545

    animation code added for testimonial section

--- static/javaScript/landingPage.js
+++ static/javaScript/landingPage.js
@@ -1,2 +1,19 @@
 'use strict';
-$('.book_img_rotator').hiSlide();
+
+$('.book_img_rotator').hiSlide(); //best selling books animation
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    const revealCard = document.querySelectorAll('.card');
+
+    revealCard.forEach(card => {
+        let windowheight = window.innerHeight;
+        let revealtop = card.getBoundingClientRect().top; //cards top value on the window
+        const revealPoint = 150;
+
+        if(revealtop < windowheight - revealPoint) {
+            card.classList.add('active');
+        }
+    })
+}
