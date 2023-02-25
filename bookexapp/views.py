436a3982f1d326726e4275c0ac1c1fc8509aae72
shrijanlakhey <lakheyshrijan@gmail.com>
2023-02-25 11:11:10 +0545

    attached html file to email sent to new users after signing up

--- bookexapp/views.py
+++ bookexapp/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import HttpResponse
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from django.utils.html import strip_tags
 # Create your views here.
 
 # basic views only to check if it works
@@ -19,16 +22,28 @@ def signup(request):
             user.save()
 
             signed = True
-
             
-            send_mail(
-                'Welcome', # subject
-                'Welcome to our website', # message
-                'disyou2134@gmail.com',
-                [user.email],
-                fail_silently=False,
-            )
+            # send_mail(
+            #     'Welcome', # subject
+            #     'Welcome to our website', # message
+            #     'disyou2134@gmail.com',
+            #     [user.email],
+            #     fail_silently=False,
+            # )
 
+            subject = 'Welcome'
+            html_content = render_to_string('bookexapp/welcomemail.html',{'name':user.username})
+            from_email = 'disyou2134@gmail.com'
+            to = user.email
+            text_content = strip_tags(html_content)
+            email = EmailMultiAlternatives(
+                subject,
+                text_content,
+                from_email,
+                [to],   
+            )
+            email.attach_alternative(html_content,"text/html")
+            email.send()
 
         else:
             print(user_form.errors)
